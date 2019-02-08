174835f15dda46520ec555ced86cae17d8a2243c
prembhusal <prembhusal@10.16.48.203>
2019-02-08 12:08:38 -0500

    setter Method

--- SpringProj/src/org/first/Organization.java
+++ SpringProj/src/org/first/Organization.java
@@ -2,7 +2,13 @@ package org.first;
 
 public class Organization {
 	HotItem hotItem;
+	/*//constructor method
 	Organization(HotItem hotDrink) {
+		this.hotItem = hotDrink;
+	}*/
+	
+	//setter method
+	public  void setHotItem(HotItem hotDrink) {
 		this.hotItem = hotDrink;
 	}
 	public void prepareHotDrink() {
